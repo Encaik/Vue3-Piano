476b0e86595e9b5d4ccbb19a7b31bda0e0082af8
shatong <843391622@qq.com>
2021-05-29 14:50:12 +0800

    feat：添加乐谱演奏

--- src/App.vue
+++ src/App.vue
@@ -2,176 +2,200 @@
   <div id="App">
     <div class="container">
       <div class="title">ENCAIK POINA</div>
-      <div class="keyboard">
-        <div
-          :class="key.type?'black-key':'white-key'"
-          v-for="key in keyboardTop"
-          :key="key.name"
-          @click="playKey(key.name)"
-        >{{key.name}}</div>
+      <div class="setting">
+        <div class="setting-btn-group">
+          <div
+            class="setting-btn"
+            @click="onNameTypeSet(0)"
+          >键名</div>
+          <div
+            class="setting-btn"
+            @click="onNameTypeSet(1)"
+          >音名</div>
+        </div>
       </div>
       <div class="keyboard">
         <div
-          :class="key.type?'black-key':'white-key'"
-          v-for="key in keyboardBottom"
-          :key="key.name"
-          @click="playKey(key.name)"
-        >{{key.name}}</div>
+          :class="key.keyType?'black-key':'white-key'"
+          v-for="key in keyboard.slice(0,keyboard.length/2)"
+          :key="key.pitchName"
+          @click="playKey(key.pitchName)"
+          :id="key.pitchName"
+        >{{nameType?key.pitchName:key.keyName}}</div>
+      </div>
+      <div class="keyboard">
+        <div
+          :class="key.keyType?'black-key':'white-key'"
+          v-for="key in keyboard.slice(keyboard.length/2)"
+          :key="key.pitchName"
+          @click="playKey(key.pitchName)"
+          :id="key.pitchName"
+        >{{nameType?key.pitchName:key.keyName}}</div>
+      </div>
+      <div class="text-input">
+        <span>C4中央Do</span>
+        <span>R四分休止符</span>
+        <span>_（下划线）四分延音符</span>
+        <span>-（减号）八分音符</span>
+        <span>=（等号）十六分音符</span>
+        <span>.（英文句号）附点音符</span>
+      </div>
+      <div class="music-input">
+        <textarea
+          rows="10"
+          cols="200"
+          v-model="musicStr"
+        >
+        </textarea>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import * as Tone from 'tone';
-let keyboardTop = [
-  { name: "A1", type: 0 },
-  { name: "Bb1", type: 1 },
-  { name: "B1", type: 0 },
-  { name: "C2", type: 0 },
-  { name: "Db2", type: 1 },
-  { name: "D2", type: 0 },
-  { name: "Eb2", type: 1 },
-  { name: "E2", type: 0 },
-  { name: "F2", type: 0 },
-  { name: "Gb2", type: 1 },
-  { name: "G2", type: 0 },
-  { name: "Ab2", type: 1 },
-  { name: "A2", type: 0 },
-  { name: "Bb2", type: 1 },
-  { name: "B2", type: 0 },
-  { name: "C3", type: 0 },
-  { name: "Db3", type: 1 },
-  { name: "D3", type: 0 },
-  { name: "Eb3", type: 1 },
-  { name: "E3", type: 0 },
-  { name: "F3", type: 0 },
-  { name: "Gb3", type: 1 },
-  { name: "G3", type: 0 },
-  { name: "Ab3", type: 1 },
-  { name: "A3", type: 0 },
-  { name: "Bb3", type: 1 },
-  { name: "B3", type: 0 },
-  { name: "C4", type: 0 },
-  { name: "Db4", type: 1 },
-  { name: "D4", type: 0 },
-  { name: "Eb4", type: 1 },
-  { name: "E4", type: 0 },
-  { name: "F4", type: 0 },
-  { name: "Gb4", type: 1 },
-  { name: "G4", type: 0 },
-  { name: "Ab4", type: 1 },
-  { name: "A4", type: 0 },
-  { name: "Bb4", type: 1 },
-  { name: "B4", type: 0 },
-  { name: "C5", type: 0 },
-  { name: "Db5", type: 1 },
-  { name: "D5", type: 0 },
-  { name: "Eb5", type: 1 },
-  { name: "E5", type: 0 }
+//键盘渲染对象
+let keyboard = [
+  { keyName: "", pitchName: "A0", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Bb0", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "B0", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "C1", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Db1", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "D1", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Eb1", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "E1", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "F1", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Gb1", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "G1", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Ab1", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "A1", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Bb1", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "B1", keyType: 0, keyMap: "" },
+  { keyName: "Q", pitchName: "C2", keyType: 0, keyMap: "KeyQ" },
+  { keyName: "2", pitchName: "Db2", keyType: 1, keyMap: "Digit2" },
+  { keyName: "w", pitchName: "D2", keyType: 0, keyMap: "KeyW" },
+  { keyName: "3", pitchName: "Eb2", keyType: 1, keyMap: "Digit3" },
+  { keyName: "E", pitchName: "E2", keyType: 0, keyMap: "KeyE" },
+  { keyName: "R", pitchName: "F2", keyType: 0, keyMap: "KeyR" },
+  { keyName: "5", pitchName: "Gb2", keyType: 1, keyMap: "Digit5" },
+  { keyName: "T", pitchName: "G2", keyType: 0, keyMap: "KeyT" },
+  { keyName: "6", pitchName: "Ab2", keyType: 1, keyMap: "Digit6" },
+  { keyName: "Y", pitchName: "A2", keyType: 0, keyMap: "KeyY" },
+  { keyName: "7", pitchName: "Bb2", keyType: 1, keyMap: "Digit7" },
+  { keyName: "U", pitchName: "B2", keyType: 0, keyMap: "KeyU" },
+  { keyName: "I", pitchName: "C3", keyType: 0, keyMap: "KeyI" },
+  { keyName: "9", pitchName: "Db3", keyType: 1, keyMap: "Digit9" },
+  { keyName: "O", pitchName: "D3", keyType: 0, keyMap: "KeyO" },
+  { keyName: "0", pitchName: "Eb3", keyType: 1, keyMap: "Digit0" },
+  { keyName: "P", pitchName: "E3", keyType: 0, keyMap: "KeyP" },
+  { keyName: "[", pitchName: "F3", keyType: 0, keyMap: "BracketLeft" },
+  { keyName: "=", pitchName: "Gb3", keyType: 1, keyMap: "Equal" },
+  { keyName: "]", pitchName: "G3", keyType: 0, keyMap: "BracketRight" },
+  { keyName: "A", pitchName: "Ab3", keyType: 1, keyMap: "KeyA" },
+  { keyName: "Z", pitchName: "A3", keyType: 0, keyMap: "KeyZ" },
+  { keyName: "S", pitchName: "Bb3", keyType: 1, keyMap: "KeyS" },
+  { keyName: "X", pitchName: "B3", keyType: 0, keyMap: "KeyX" },
+  { keyName: "C", pitchName: "C4", keyType: 0, keyMap: "KeyC" },
+  { keyName: "F", pitchName: "Db4", keyType: 1, keyMap: "KeyF" },
+  { keyName: "V", pitchName: "D4", keyType: 0, keyMap: "KeyV" },
+  { keyName: "G", pitchName: "Eb4", keyType: 1, keyMap: "KeyG" },
+  { keyName: "B", pitchName: "E4", keyType: 0, keyMap: "KeyB" },
+  { keyName: "N", pitchName: "F4", keyType: 0, keyMap: "KeyN" },
+  { keyName: "J", pitchName: "Gb4", keyType: 1, keyMap: "KeyJ" },
+  { keyName: "M", pitchName: "G4", keyType: 0, keyMap: "KeyM" },
+  { keyName: "K", pitchName: "Ab4", keyType: 1, keyMap: "KeyK" },
+  { keyName: ",", pitchName: "A4", keyType: 0, keyMap: "Comma" },
+  { keyName: "L", pitchName: "Bb4", keyType: 1, keyMap: "KeyL" },
+  { keyName: ".", pitchName: "B4", keyType: 0, keyMap: "Period" },
+  { keyName: "/", pitchName: "C5", keyType: 0, keyMap: "Slash" },
+  { keyName: "'", pitchName: "Db5", keyType: 1, keyMap: "Quote" },
+  { keyName: "", pitchName: "D5", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Eb5", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "E5", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "F5", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Gb5", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "G5", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Ab5", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "A5", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Bb5", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "B5", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "C6", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Db6", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "D6", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Eb6", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "E6", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "F6", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Gb6", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "G6", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Ab6", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "A6", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Bb6", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "B6", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "C7", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Db7", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "D7", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Eb7", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "E7", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "F7", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Gb7", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "G7", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Ab7", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "A7", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "Bb7", keyType: 1, keyMap: "" },
+  { keyName: "", pitchName: "B7", keyType: 0, keyMap: "" },
+  { keyName: "", pitchName: "C8", keyType: 0, keyMap: "" }
 ];
-let keyboardBottom = [
-  { name: "F5", type: 0 },
-  { name: "Gb5", type: 1 },
-  { name: "G5", type: 0 },
-  { name: "Ab5", type: 1 },
-  { name: "A5", type: 0 },
-  { name: "Bb5", type: 1 },
-  { name: "B5", type: 0 },
-  { name: "C6", type: 0 },
-  { name: "Db6", type: 1 },
-  { name: "D6", type: 0 },
-  { name: "Eb6", type: 1 },
-  { name: "E6", type: 0 },
-  { name: "F6", type: 0 },
-  { name: "Gb6", type: 1 },
-  { name: "G6", type: 0 },
-  { name: "Ab6", type: 1 },
-  { name: "A6", type: 0 },
-  { name: "Bb6", type: 1 },
-  { name: "B6", type: 0 },
-  { name: "C7", type: 0 },
-  { name: "Db7", type: 1 },
-  { name: "D7", type: 0 },
-  { name: "Eb7", type: 1 },
-  { name: "E7", type: 0 },
-  { name: "F7", type: 0 },
-  { name: "Gb7", type: 1 },
-  { name: "G7", type: 0 },
-  { name: "Ab7", type: 1 },
-  { name: "A7", type: 0 },
-  { name: "Bb7", type: 1 },
-  { name: "B7", type: 0 },
-  { name: "C8", type: 0 },
-  { name: "Db8", type: 1 },
-  { name: "D8", type: 0 },
-  { name: "Eb8", type: 1 },
-  { name: "E8", type: 0 },
-  { name: "F8", type: 0 },
-  { name: "Gb8", type: 1 },
-  { name: "G8", type: 0 },
-  { name: "Ab8", type: 1 },
-  { name: "A8", type: 0 },
-  { name: "Bb8", type: 1 },
-  { name: "B8", type: 0 },
-  { name: "C9", type: 0 }
-];
-let music = [
-  { pitch: "E5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "Eb5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "E5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "Eb5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "E5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "B4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "D5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "C5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "A4", pitchTime: 2, playTime: 0.75 },
-  { pitch: "C4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "E4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "A4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "B4", pitchTime: 2, playTime: 0.75 },
-  { pitch: "E4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "Ab4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "B4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "C5", pitchTime: 2, playTime: 0.75 },
-  { pitch: "E4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "E5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "Eb5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "E5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "Eb5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "E5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "B4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "D5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "C5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "A4", pitchTime: 2, playTime: 0.75 },
-  { pitch: "C4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "E4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "A4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "B4", pitchTime: 2, playTime: 0.75 },
-  { pitch: "E4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "C5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "B4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "A4", pitchTime: 2, playTime: 0.75 },
-  { pitch: "B4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "C5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "D5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "E5", pitchTime: 2, playTime: 0.75 },
-  { pitch: "G4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "F5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "E5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "D5", pitchTime: 2, playTime: 0.75 },
-  { pitch: "F4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "E5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "D5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "C5", pitchTime: 2, playTime: 0.75 },
-  { pitch: "E4", pitchTime: 2, playTime: 0.25 },
-  { pitch: "D5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "C5", pitchTime: 2, playTime: 0.25 },
-  { pitch: "B4", pitchTime: 2, playTime: 1 },
-]
+//歌谱字符串
+/**
+ * -八分音符
+ * =十六分音符
+ * -四份音符
+ * .附点音符
+ */
 
+
+let musicStr = ref("C4-D4-E4-G4G4-R-E4-G4-E4-A4-G4-E4-G4.R-E4=G4=A4-A4-A4-A4-A4-G4-F4=G4=G4._E4B4-C5C5-C5-E4-R-E4-B4-B4C5-B4-G4.R-C4=G4=A4-A4-A4-A4-A4-G4-F4=G4=G4._");
+function playMusic() {
+  let noteDuration = 0.5;
+  let music = [];//乐谱演奏对象
+  let musicArr = musicStr.value.match(/[a-zA-Z]+[0-9]?(-|=)?\.?/g) || [];
+  for (let item of musicArr) {
+    console.log(item)
+    let playTime = 0;
+    let timeArr = item.match(/(-|=|\.)/g) || [];
+    console.log(timeArr)
+    if (timeArr.includes('-')) {
+      playTime += noteDuration / 2
+    } else if (timeArr.includes('=')) {
+      playTime += noteDuration / 4
+    } else if (timeArr.includes('_')) {
+      playTime += noteDuration
+    } else {
+      playTime += noteDuration
+    }
+    if (timeArr.includes('.')) {
+      playTime += playTime / 2
+    }
+    music.push({
+      pitch: item.match(/[a-zA-Z]+[0-9]?/)[0],
+      playTime,
+    })
+  }
+  let now = Tone.now();
+  music.forEach(i => {
+    console.log(i);
+    if (i.pitch !== "R") {
+      sampler.triggerAttackRelease(i.pitch, "2n", now);
+    }
+    now += i.playTime;
+  })
+}
+//采样器加载
 const sampler = new Tone.Sampler({
   urls: {
+    "A0": "A0.mp3",
+    "C1": "C1.mp3",
     "A1": "A1.mp3",
     "C2": "C2.mp3",
     "A2": "A2.mp3",
@@ -183,26 +207,47 @@ const sampler = new Tone.Sampler({
     "C6": "C6.mp3",
     "A6": "A6.mp3",
     "C7": "C7.mp3",
-    "A7": "A7.mp3",
-    "C8": "C8.mp3",
   },
   release: 1,
-  baseUrl: "https://tonejs.github.io/audio/salamander/",
+  baseUrl: "/samples/piano/",
 }).toDestination();
-
-
-
+//采样器加载完成后演奏乐谱
 Tone.loaded().then(() => {
-  let now = Tone.now()
-  music.forEach(i => {
-    sampler.triggerAttackRelease(i.pitch, `${i.pitchTime}n`, now);
-    now += i.playTime;
-  })
+  playMusic();
+})
+//单音演奏方法
+function playKey(pitchName) {
+  sampler.triggerAttackRelease(pitchName, "8n");
+  let $el = document.getElementById(pitchName)
+  $el.classList.add("key-active");
+  setTimeout(() => {
+    $el.classList.remove("key-active");
+  }, 100)
+}
+
+/* 设置 */
+// 名称显示
+let nameType = ref(1);
+
+function onNameTypeSet(type) {
+  nameType.value = type;
+}
+
+
+/*  键盘演奏逻辑 */
+let pressKeys = new Map();
+document.addEventListener("keydown", (event) => {
+  //console.log(event);
+  if (pressKeys.has(event.code)) return;
+  pressKeys.set(event.code)
+  let key = keyboard.filter(i => i.keyMap === event.code)?.[0];
+  key ? playKey(key.pitchName) : null;
+})
+document.addEventListener("keyup", (event) => {
+  pressKeys.delete(event.code)
 })
 
-function playKey(key) {
-  sampler.triggerAttackRelease(key, "8n");
-}
+/* 鼠标演奏逻辑 */
 let isPlay = false;
 document.addEventListener("mousedown", () => {
   isPlay = true;
@@ -215,6 +260,7 @@ document.addEventListener("mouseover", (event) => {
 document.addEventListener("mouseup", () => {
   isPlay = false;
 })
+
 </script>
 
 <style lang="scss">
@@ -238,6 +284,30 @@ document.addEventListener("mouseup", () => {
   filter: drop-shadow(4px 4px 4px #2e2e2e);
   margin-bottom: 25px;
 }
+.setting {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .setting-btn-group {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .setting-btn {
+      background-color: #4285f4;
+      color: #fff;
+      width: 120px;
+      height: 32px;
+      line-height: 32px;
+      border-radius: 2px;
+      font-size: 0.9em;
+      text-align: center;
+      cursor: pointer;
+      &:hover {
+        background-color: #1f50a0;
+      }
+    }
+  }
+}
 .keyboard {
   display: flex;
   justify-content: center;
@@ -251,6 +321,11 @@ document.addEventListener("mouseup", () => {
     user-select: none;
     cursor: pointer;
     font-size: 8px;
+    transition: all 0.2s ease-out;
+    &:hover {
+      background-color: #747474;
+      box-shadow: 2px 2px 0px 0px #747474;
+    }
   }
   .white-key {
     @include key;
@@ -259,17 +334,6 @@ document.addEventListener("mouseup", () => {
     background-color: #fff;
     border: 2px solid gray;
     box-shadow: 2px 2px 0px 0px #fff;
-    &:hover {
-      background-color: #d6d6d6;
-      box-shadow: 2px 2px 0px 0px #d6d6d6;
-    }
-    &:active {
-      background-color: rgb(177, 177, 177);
-      box-shadow: 2px 2px 0px 0px rgb(177, 177, 177);
-    }
-    .white-key + .white-key {
-      margin-right: -1px;
-    }
   }
   .black-key {
     @include key;
@@ -281,14 +345,22 @@ document.addEventListener("mouseup", () => {
     z-index: 10;
     color: #fff;
     box-shadow: 2px 2px 0px 0px #000;
-    &:hover {
-      background-color: #2e2e2e;
-      box-shadow: 2px 2px 0px 0px #2e2e2e;
-    }
-    &:active {
-      background-color: rgb(80, 80, 80);
-      box-shadow: 2px 2px 0px 0px rgb(80, 80, 80);
-    }
   }
+  .key-active {
+    background-color: #575757;
+    box-shadow: 2px 2px 0px 0px #575757;
+  }
+}
+.text-input {
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+  margin: 25px 0;
+  color: #fff;
+  font-size: 20px;
+}
+.music-input {
+  display: flex;
+  justify-content: center;
 }
 </style>
